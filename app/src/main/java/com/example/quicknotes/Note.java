3f1adaabdbdb2ea8234f1e1d1016377f094084fc
jpkhawam <jeanpaulkhawam2001@gmail.com>
2022-01-29 19:15:38 +0200

    formatted the comments better

--- app/src/main/java/com/example/quicknotes/Note.java
+++ app/src/main/java/com/example/quicknotes/Note.java
@@ -3,12 +3,12 @@ package com.example.quicknotes;
 public class Note {
 
     private String title;
-    /* // TODO: Possible attributes to be implemented
-    private Color backgroundColor;
-    private Date dateCreated;
-    private Date dateEdited;
-    private Integer noteIdentifier;
-    */
+
+    // TODO: Possible attributes to be implemented
+    // private Color backgroundColor;
+    // private Date dateCreated;
+    // private Date dateEdited;
+    // private Integer noteIdentifier;
 
     public String getTitle() {
         return title;
