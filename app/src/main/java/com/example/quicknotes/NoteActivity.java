fe30100080526d1a7a4c978a7af92699c3ff5d5a
Jean-Paul Khawam <jeanpaulkhawam2001@gmail.com>
2022-02-11 17:07:40 +0200

    Added bottom bar inside note_layout

--- app/src/main/java/com/example/quicknotes/NoteActivity.java
+++ app/src/main/java/com/example/quicknotes/NoteActivity.java
@@ -2,15 +2,14 @@ package com.example.quicknotes;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.appbar.MaterialToolbar;
 
 public class NoteActivity extends AppCompatActivity {
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,23 +19,19 @@ public class NoteActivity extends AppCompatActivity {
         topAppBar.setNavigationOnClickListener(view -> {
             // go to home page
         });
-        topAppBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
-            @SuppressLint("NonConstantResourceId")
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.note_pin:
-                        // pin note
-                        return true;
-                    case R.id.note_add_reminder:
-                        // add reminder
-                        return true;
-                    case R.id.note_send_to_archive:
-                        // send to archive
-                        return true;
-                    default:
-                        return false;
-                }
+        topAppBar.setOnMenuItemClickListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.note_pin:
+                    // pin note
+                    return true;
+                case R.id.note_add_reminder:
+                    // add reminder
+                    return true;
+                case R.id.note_send_to_archive:
+                    // send to archive
+                    return true;
+                default:
+                    return false;
             }
         });
     }
