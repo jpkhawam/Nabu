c0287a2120c972b47dad548168e51f182a12acc7
Jean-Paul Khawam <jeanpaulkhawam2001@gmail.com>
2022-02-12 18:43:54 +0200

    passing data via intents

--- app/src/main/java/com/example/quicknotes/NoteActivity.java
+++ app/src/main/java/com/example/quicknotes/NoteActivity.java
@@ -12,6 +12,10 @@ import com.google.android.material.bottomappbar.BottomAppBar;
 
 public class NoteActivity extends AppCompatActivity {
 
+    public static final String NOTE_TITLE_KEY = "noteTitle";
+    public static final String NOTE_CONTENT_KEY = "noteContent";
+    public static final String NOTE_IDENTIFIER_KEY = "noteIdentifier";
+
     @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +24,19 @@ public class NoteActivity extends AppCompatActivity {
 
         EditText editTextTitle = findViewById(R.id.input_note_title);
         EditText editTextContent = findViewById(R.id.input_note_content);
+        int noteIdentifier;
+
+        Intent intentReceived = getIntent();
+        if (intentReceived != null) {
+            editTextTitle.setText(intentReceived.getStringExtra(NOTE_TITLE_KEY));
+            editTextContent.setText(intentReceived.getStringExtra(NOTE_CONTENT_KEY));
+            noteIdentifier = intentReceived.getIntExtra(NOTE_IDENTIFIER_KEY, -1);
+        }
 
         MaterialToolbar topAppBar = findViewById(R.id.noteTopBar);
         topAppBar.setNavigationOnClickListener(view -> {
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
+            Intent outgoingIntent = new Intent(this, MainActivity.class);
+            startActivity(outgoingIntent);
         });
         topAppBar.setOnMenuItemClickListener(item -> {
             switch (item.getItemId()) {
